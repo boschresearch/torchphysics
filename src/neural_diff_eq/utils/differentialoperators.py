d89976b95bdedf16e0a7f9f9c2cf0a9fa65305a6
Tom Freudenberg <tomfre@briareos.math.uni-bremen.de>
2021-05-25 16:41:52 +0200

    cleaner docs
    Signed-off-by: Tom Freudenberg <tomfre@briareos.math.uni-bremen.de>

--- src/neural_diff_eq/utils/differentialoperators.py
+++ src/neural_diff_eq/utils/differentialoperators.py
@@ -4,17 +4,26 @@ import torch
 
 def laplacian(model_out, deriv_variable_input):
     '''Computes the laplacian of a network in respect to the given variable 
+
+    Parameters
+    ----------
     model_out : torch.tensor
         The output tensor of the neural network 
     deriv_variable_input : torch.tensor
         The input tensor of the variable in which respect the derivatives have to
         be computed
+
+    Returns 
+    ----------
+    torch.tensor
+        A Tensor, where every row contains the value of the sum of the second derivatives (laplace)
+        w.r.t the row of the input variable.  
     '''
     laplacian = torch.zeros((deriv_variable_input.shape[0],1))
     Du = torch.autograd.grad(model_out.sum(), deriv_variable_input, create_graph=True)[0]
-    # If function is linear in the variable we have to check, or else we get an error
-    if Du.grad_fn is None:
-        return laplacian
+    # We have to check if the model is linear w.r.t the variable, or else we get an error
+    # when we compute the second derivative. If it is linear we can just return torch.zeros 
+    if Du.grad_fn is None: return laplacian
     D2u = torch.autograd.grad(Du.sum(), deriv_variable_input, create_graph=True)[0]
     for i in range(deriv_variable_input.shape[1]):
         laplacian += D2u.narrow(1,i,1)
