fix gradient computation in validation
Signed-off-by: Nick Heilenkötter <nick7@uni-bremen.de>

src/neural_diff_eq/models/fcn.py
@@ -21,7 +21,7 @@ class SimpleFCN(DiffEqModel):
 
     def __init__(self, input_dim, depth=5, width=100, output_dim=1):
         super().__init__()
-        
+
         # build model
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(input_dim, width))

src/neural_diff_eq/problem/condition.py
@@ -9,13 +9,15 @@ from .data import Dataset
 
 class Condition(torch.nn.Module):
     def __init__(self, name, norm, weight=1.0,
-                 batch_size=1000, num_workers=0):
+                 batch_size=1000, num_workers=0,
+                 requires_input_grad=True):
         super().__init__()
         self.name = name
         self.norm = norm
         self.weight = weight
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.requires_input_grad = requires_input_grad
 
         # variables are registered when the condition is added to a problem or variable
         self.variables = None
@@ -65,7 +67,8 @@ class DataCondition(Condition):
                  weight=1.0, batch_size=1000, num_workers=2):
         super().__init__(name, norm, weight,
                          batch_size=batch_size,
-                         num_workers=num_workers)
+                         num_workers=num_workers,
+                         requires_input_grad=False)
 
     def forward(self, model, data):
         data, target = data
@@ -77,9 +80,10 @@ class DataCondition(Condition):
 
 
 class BoundaryCondition(Condition):
-    def __init__(self, name, norm, weight, batch_size, num_workers):
+    def __init__(self, name, norm, weight, batch_size, num_workers, requires_input_grad):
         super().__init__(name, norm, weight=weight, batch_size=batch_size,
-                         num_workers=num_workers)
+                         num_workers=num_workers,
+                         requires_input_grad=requires_input_grad)
         # boundary_variable is registered when the condition is added to that variable
         self.boundary_variable = None  # string
 
@@ -89,7 +93,7 @@ class DirichletCondition(BoundaryCondition):
                  sampling_strategy='random', weight=1.0, batch_size=1000,
                  num_workers=0, dataset_size=10000):
         super().__init__(name, norm, weight=weight, batch_size=batch_size,
-                         num_workers=num_workers)
+                         num_workers=num_workers, requires_input_grad=False)
         self.dirichlet_fun = dirichlet_fun
         self.sampling_strategy = sampling_strategy
         self.dataset_size = dataset_size

src/neural_diff_eq/solver.py
@@ -60,21 +60,26 @@ class PINNModule(pl.LightningModule):
         dataloader_dict = self._get_dataloader(self.problem.get_val_conditions())
         return pl.trainer.supporters.CombinedLoader(dataloader_dict, 'max_size_cycle')
 
-    def _do_step(self, conditions, batch, batch_idx):
-        loss = torch.Tensor(0.)
+    def training_step(self, batch, batch_idx):
+        loss = torch.zeros(1, requires_grad=True)
+        conditions = self.problem.get_train_conditions()
         for name in conditions:
             data = batch[name]
             c = conditions[name](self.model, data)
             self.log(name, c)
-            loss += conditions[name].weight * c
+            loss = loss + conditions[name].weight * c
         # TODO: should be extended by custom metric logging in future
         # NOTE: we should clarify whether multiple conditions with derivatives
         #       lead to multiple computations of those derivatives,
         #       if yes, there could be more efficient solutions
         return loss
 
-    def training_step(self, batch, batch_idx):
-        return self._do_step(self.problem.get_train_conditions(), batch, batch_idx)
-
     def validation_step(self, batch, batch_idx):
-        self._do_step(self.problem.get_val_conditions(), batch, batch_idx)
+        loss = torch.zeros(1)
+        conditions = self.problem.get_train_conditions()
+        for name in conditions:
+            torch.set_grad_enabled(conditions[name].requires_input_grad)
+            data = batch[name]
+            c = conditions[name](self.model, data)
+            self.log(name, c)
+            loss = loss + conditions[name].weight * c