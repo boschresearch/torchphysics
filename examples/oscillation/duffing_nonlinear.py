642db6267c67e44fa44146f910bff0894efdb9e1
DE\KRD2RNG <DanielChristopher.Kreuter@de.bosch.com>
2022-05-30 13:19:46 +0200

    duffing ready for improvements

--- examples/oscillation/duffing_nonlinear.py
+++ examples/oscillation/duffing_nonlinear.py
@@ -10,13 +10,11 @@
 
 import torch
 import torchphysics as tp
-import math
 import pytorch_lightning as pl
 import pandas as pd
 import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.interpolate import InterpolatedUnivariateSpline as IUS
-
 # In[19]:
 omega_0 = 1
 
@@ -24,7 +22,7 @@ ode_params = {'D': .05,      # damping constant
               'c_nlin': 0.2  # nonlinearity
              }
 sampling_frequency = 10 
-t_end = 500 # sec
+t_end = 250 # sec
 time = np.linspace(0, t_end, t_end*sampling_frequency + 1)
 time_ind = pd.to_timedelta(time, unit="sec")
 
@@ -73,28 +71,29 @@ Dmin, Dmax = 0.01, 0.1
 A_t = tp.domains.Interval(T, 0, t_end)
 A_D = tp.domains.Interval(D, Dmin, Dmax)
 A_cln = tp.domains.Interval(C_nl, 0, 0.5)
-inner_sampler = (tp.samplers.GridSampler(A_t, n_points = 2500) \
-                 *(tp.samplers.GridSampler(A_D, n_points = 10)\
-                 *(tp.samplers.GridSampler(A_cln, n_points = 10)
+inner_sampler = (tp.samplers.GridSampler(A_t, n_points = 5000) \
+                 *(tp.samplers.GridSampler(A_D, n_points = 5)\
+                 *(tp.samplers.GridSampler(A_cln, n_points = 5)
                  ))).make_static()
     
 initial_u_sampler = (tp.samplers.GridSampler(A_t.boundary_left, n_points = 1) \
-                        *(tp.samplers.GridSampler(A_D, n_points = 10)\
-                        *(tp.samplers.GridSampler(A_cln, n_points = 10)
+                        *(tp.samplers.GridSampler(A_D, n_points = 5)\
+                        *(tp.samplers.GridSampler(A_cln, n_points = 5)
                         ))).make_static()
 initial_v_sampler = (tp.samplers.GridSampler(A_t.boundary_left, n_points = 1) \
-                        *(tp.samplers.GridSampler(A_D, n_points = 10)\
-                        *(tp.samplers.GridSampler(A_cln, n_points = 10)
+                        *(tp.samplers.GridSampler(A_D, n_points = 5)\
+                        *(tp.samplers.GridSampler(A_cln, n_points = 5)
                         ))).make_static()
     
 # tp.utils.scatter(T * D * C_nl, inner_sampler)
 #%% Model Definition
 model = tp.models.Sequential(
     tp.models.NormalizationLayer(A_t * A_D * A_cln),
-    tp.models.FCN(input_space=T * D * C_nl, output_space=U, hidden=(15,15)))
+    tp.models.FCN(input_space=T * D * C_nl, output_space=U, hidden=(20,20)))
 
 def duffing_residual(u, t, D, C_nl):
-    return tp.utils.laplacian(u, t)  + 2 * D * tp.utils.grad(u, t) + u + C_nl * u**3 - F_inter_func(t)
+    rhs = torch.tensor(F_inter_func(t.detach().numpy()))
+    return tp.utils.laplacian(u, t)  + 2 * D * tp.utils.grad(u, t) + u + C_nl * u**3 - rhs
 pde_condition = tp.conditions.PINNCondition(module=model,
                                             sampler=inner_sampler,
                                             residual_fn=duffing_residual,
@@ -124,21 +123,38 @@ initial_v_condition = tp.conditions.PINNCondition(module=model,
 #                                     data_functions={'f': right_hand_side},
 #                                     name='force')
 #%% Training
-opt_setting = tp.solver.OptimizerSetting(torch.optim.AdamW, lr=1e-2) #AdamW, SGD, LBFGS
+opt_setting = tp.solver.OptimizerSetting(torch.optim.LBFGS, lr=1e-2) #AdamW, SGD, LBFGS
 solver = tp.solver.Solver([pde_condition,
                            initial_u_condition,
                            initial_v_condition], optimizer_setting = opt_setting)
 
 trainer = pl.Trainer(gpus=None, # or 1 if GPU available
-                     max_steps = 200,
-                     #logger=False, 
+                     max_steps = 5000,
+                     logger=True, 
                      benchmark=True,
                      log_every_n_steps=1,
                      )
 trainer.fit(solver)
+#%% Prediction
+time_tensor = torch.tensor(time)
+D_tensor = ode_params["D"] * torch.ones(len(time_tensor) + 1, 1)
+cln_tensor = ode_params["c_nlin"] * torch.ones(len(time_tensor) + 1, 1)
 
+grid_sampler = tp.samplers.GridSampler(A_t, n_points=len(time_tensor)) + tp.samplers.GridSampler(A_t.boundary, n_points=1)
+grid_points = grid_sampler.sample_points()
+eval_points = grid_points.join(tp.spaces.Points.from_coordinates({
+    # 't': time_tensor,
+    'D': D_tensor,
+    'C_nl': cln_tensor
+    }
+    )
 
-
+                        )
+model_out = model(eval_points)
+pinn_solu = pd.Series(model_out.as_tensor.detach().numpy().ravel(),
+                      index=pd.Index(grid_points.as_tensor.detach().numpy().ravel(),
+                                     name="time"))
+pinn_solu.plot()
 
 
 
